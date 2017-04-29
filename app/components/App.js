e1814a0a8a131e26dccad84fa4762ab0a5af097c
Sarah Schneider <rifkegribenes@gmail.com>
2017-04-29 14:17:51 -0700

    initial commit

---
+++ app/components/App.js
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var App = React.createClass({
+    render: function () {
+        return ( <h1>
+            is this thing on ?
+            </h1>
+                <button>
+            Click Me!
+            </button>
+        );
+    }
+});
+
+module.exports = App;
